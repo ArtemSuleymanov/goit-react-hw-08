a6d8ec00aefc3fadca1a38f2d35d339ac59a5219
Artem <suleymanov2007artem@gmail.com>
2025-03-01 20:07:30 +0200

    update store and filter selectors

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,19 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import contactsReducer from "./contacts/slice";
 import filtersReducer from "./filter/slice";
+import { authReducer } from "./auth/slice";
+
+const authPersistConfig = {
+  key: "auth",
+  storage,
+  whitelist: ["token"],
+};
 
 export const store = configureStore({
   reducer: {
+    auth: persistReducer(authPersistConfig, authReducer),
     contacts: contactsReducer,
     filters: filtersReducer,
   },
@@ -12,3 +22,5 @@ export const store = configureStore({
       serializableCheck: false,
     }),
 });
+
+export const persistor = persistStore(store);
